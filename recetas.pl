4d8408b4e3241968980850be619f084f8d47a836
Josh Jimenez <jjl1089@gmail.com>
2019-06-05 00:12:01 -0600

    Update recetas.pl
    formato de nombres con ' _ '

--- recetas.pl
+++ recetas.pl
@@ -1,9 +1,9 @@
-receta('puerco asado','cena',['puerco','sal','pimienta'],'embarre el puerco de sal y pimienta y pongalo al fuego',['chanchoalhorno.png']).
-receta('arroz con huevo','almuerzo',['arroz','huevo'],'ponga a hacer el huevo como si fuese a ser pateado y agrega el arroz',['arrozconuevo.png']).
-receta('sopa negra','cena',['frijoles','huevo'],'haga frijoles y agregue el huevo sin pelar para que se ponga duro', ['sopanegra.png']).
+receta('puerco_asado','cena',['puerco','sal','pimienta'],'embarre el puerco de sal y pimienta y pongalo al fuego',['chanchoalhorno.png']).
+receta('arroz_con_huevo','almuerzo',['arroz','huevo'],'ponga a hacer el huevo como si fuese a ser pateado y agrega el arroz',['arrozconuevo.png']).
+receta('sopa_negra','cena',['frijoles','huevo'],'haga frijoles y agregue el huevo sin pelar para que se ponga duro', ['sopanegra.png']).
 receta('cereal', 'desayuno',['cereal','leche'],'ponga el cereal en un plato hondo y agregue la leche',['cereal.png']).
-receta('macarrones','almuerzo',['espaguetti','salsa de tomate','queso'],'cocine los espaguetties, agregue la salsa y luego el queso',['macarrones.png']).
-receta('arepas','desayuno',['leche','polvito de arepa','azucar','huevo'],'haga una masa con el polvo, la leche y el huevo, agrega azucar al gusto y frie arepitas en el sarten',['arepa.png']).
+receta('macarrones','almuerzo',['espaguetti','salsa_de_tomate','queso'],'cocine los espaguetties, agregue la salsa y luego el queso',['macarrones.png']).
+receta('arepas','desayuno',['leche','polvito_de_arepa','azucar','huevo'],'haga una masa con el polvo, la leche y el huevo, agrega azucar al gusto y frie arepitas en el sarten',['arepa.png']).
 
 %Busquedas
 
